91dd1af167c98f6b32d2b7f1b141603deef3d038
philipexd <79484293+philipexd@users.noreply.github.com>
2023-04-03 21:46:27 -0300

    atualização

---
+++ C#BootCampDio/modulo2C#/Models/Pessoa.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace modulo2C.Models
+{
+    public class Pessoa // criei a classe pessoa com as propriedade nome e idade 
+    {
+        private string _nome; //so e permitido acessar dentro da propria classe
+        
+        private int _idade; //so e permitido acessar dentro da propria classe
+        
+        
+        public string Nome // qualquer um pode instaciar a propriedade  
+        {
+        //    get => _nome.ToUpper();
+           
+        //    set => _nome = value;
+           
+           
+            get
+            {
+                return _nome.ToUpper();// transformar em maiuscolo .Toupper
+            }
+
+
+             set
+            {
+                if (value == "")// value argumento que esta recebendo o nome 
+                {
+                    throw new ArgumentException("o nome não pode ser vazio!");
+                }
+
+                 _nome = value;// atribuir o valor no
+            
+            
+            
+            }
+
+
+
+        }//get e set significa 2 ações get obter, o valor e set e passar o valor
+
+        public string SobreNome { get; set; }
+
+        public string NomeCompleto =>$"{Nome} {SobreNome}".ToUpper();// aqui não pode se modificar so ler
+        public int Idade 
+        {
+            get => _idade;
+            
+            set
+            {
+
+                if(value < 0)
+                {
+                    throw new ArgumentException("a idade não pode ser menor que 0 ");
+                }
+
+                _idade = value;
+
+            } 
+        }//get e set significa 2 ações get obter, o valor e set e passar o valor  
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto}");
+            Console.WriteLine($"idade: {Idade}");
+        }
+
+    }
+}
---
+++ C#BootCampDio/modulo2C#/Program.cs
@@ -0,0 +1,9 @@
+﻿using modulo2C.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "bbb";
+p1.SobreNome =  "ccc";
+p1.Idade = 22;
+p1.Apresentar();
+
